695194b440a4254d976126340dcf37211d07351e
balabeir <beer32853@gmail.com>
2020-12-02 15:52:34 +0700

    updateDB

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 const url = `mongodb+srv://testDB:<test_1234>@test.eeiy5.mongodb.net/<Test>?retryWrites=true&w=majority`;
-mongoose.connect(process.env.MONGODB_URI, {
+mongoose.connect(url, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
